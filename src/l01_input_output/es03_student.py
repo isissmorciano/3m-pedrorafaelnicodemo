14b6867b645d685a36bdcb147ce1947261cd895f
Rafael <pedrorafael.nicodemo@studenti.isissgobetti.it>
2026-02-11 13:17:46 +0100

    Es.3 Completato

---
+++ src/l01_input_output/es03_student.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    numero: int = int(input("Inserisci un numero intero: "))
+    print(f"Il numero precedente è {numero - 1} e il numero successivo è {numero + 1}.")
+
+if __name__ == "__main__":
+    main()
